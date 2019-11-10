44d67670cfff0ed320e366ee43548bd9a8ebdf34
Yam Clement <yamtzunhei@gmail.com>
2019-11-10 17:45:13 +0100

    display quote

--- Front End Libraries Projects/pomodoro-clock/src/screens/Quote.js
+++ Front End Libraries Projects/pomodoro-clock/src/screens/Quote.js
@@ -1,7 +1,41 @@
 import React from "react";
+import { connect } from "react-redux";
 
-const Quote = () => {
-  return <div></div>;
+import { fetchQuotes } from "../actions";
+
+class Quote extends React.Component {
+  displayRandomQuote = () => {
+    const randomIndex =
+      Math.floor(Math.random() * this.props.quotes.length) + 1;
+    return this.props.quotes[randomIndex].text;
+  };
+
+  componentDidMount() {
+    this.props.fetchQuotes();
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      this.props.quotes && (
+        <blockquote>
+          <p>{this.displayRandomQuote()}</p>
+          <footer></footer>
+        </blockquote>
+      )
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    quotes: state.quotes.quotes,
+    isLoading: state.quotes.isLoading,
+    error: state.quotes.error
+  };
 };
 
-export default Quote;
+export default connect(
+  mapStateToProps,
+  { fetchQuotes }
+)(Quote);
